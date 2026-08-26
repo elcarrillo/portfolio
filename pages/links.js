2be86670eb6cbe522190468f803651436cb82afe
Edgar Carrillo <edgarc.ec@gmail.com>
2026-08-25 22:15:54 -0600

    cleaned up links.js

--- pages/links.js
+++ pages/links.js
@@ -1,38 +1,49 @@
 import React, { Fragment } from 'react';
-import Link from 'next/link';
 import { Footer } from '../components/Footer';
-import { links, SEO, } from '../config/config';
+import { links, SEO } from '../config/config';
 import { Header } from '../components/Header';
 
-export default function Home() {
+export default function Links() {
   return (
     <Fragment>
       <Header seo={SEO} />
       <div className="d-flex flex-column justify-content-between bg-secondary min-vh-100">
         <div className="py-5 px-5 container text-center">
-          <img className="img-fluid my-3 card-image" width="150" height="150" src={links.image} alt="profile of elcarrillo" />
+          <img
+            className="img-fluid my-3 card-image"
+            width="150"
+            height="150"
+            src={links.image}
+            alt="Profile of Edgar Carrillo"
+          />
           <h3 className="mt-3">{links.title}</h3>
           <p>{links.description}</p>
+
           {links.cards.map((value, index) => (
-            <Button key={index} title={value.title} link={value.link} />
+            <Button
+              key={index}
+              title={value.title}
+              link={value.link}
+            />
           ))}
         </div>
+
         <Footer />
       </div>
     </Fragment>
   );
 }
 
-
 function Button({ title, link }) {
   return (
     <div className="row justify-content-center">
-      <div className="card card-work mx-sm-4 my-2" style={{ width: "20rem" }}>
-        <Link href={link}>
-          <a target="_blank" rel="noreferrer">
-            <h4 className="text-primary py-3 px-3">{title}</h4>
-          </a>
-        </Link>
+      <div
+        className="card card-work mx-sm-4 my-2"
+        style={{ width: "20rem" }}
+      >
+        <a href={link} target="_blank" rel="noreferrer">
+          <h4 className="text-primary py-3 px-3">{title}</h4>
+        </a>
       </div>
     </div>
   );
